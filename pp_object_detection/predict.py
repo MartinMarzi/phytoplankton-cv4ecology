5721b523369cfbdd0dd5216ebb516b9a4a9ff4fe
martin <marzidovsek@gmail.com>
2023-08-21 20:22:55 +0000

    run predictions

--- pp_object_detection/predict.py
+++ pp_object_detection/predict.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 model = YOLO("/home/martin/cv4e/pp_object_detection/runs/detect/train8/weights/best.pt")
 
 # Define path to the image file
-# source = "/mnt/ssd-cluster/martin/datasets/bbox/images/test/Pn_calliantha_20230523_0024_5.jpg"
+source = "/mnt/ssd-cluster/martin/datasets/bbox/images/test/Pn_delicatissima_20230601_0DB2_6.jpg"
 
 # Predict with the model
-results = model("/mnt/ssd-cluster/martin/datasets/bbox/images/test/Pn_calliantha_20230523_0024_5.jpg", show=True, save=True) 
+results = model(source, save=True) 
