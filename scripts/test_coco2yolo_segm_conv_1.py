b90743d0debd87db326b5d240decd826135654f7
Martin Marzidovsek <marzidovsek@gmail.com>
2023-08-22 14:45:54 -0700

    update conversion test

--- scripts/test_coco2yolo_segm_conv_1.py
+++ scripts/test_coco2yolo_segm_conv_1.py
@@ -12,7 +12,7 @@ def display_yolo_segmentations(image_dir, yolo_annotations_dir, image_name):
 
     img_height, img_width = image.shape[:2]
 
-    # Read YOLO annotations for the image in the image_dir
+    # Read YOLO annotations for the image
     annotation_path = os.path.join(yolo_annotations_dir, image_name + ".txt")
     with open(annotation_path, 'r') as file:
         lines = file.readlines()
@@ -20,12 +20,13 @@ def display_yolo_segmentations(image_dir, yolo_annotations_dir, image_name):
             data = line.strip().split()
             class_id = int(data[0])
             polygon_points = [(int(float(data[i]) * img_width), int(float(data[i+1]) * img_height)) for i in range(1, len(data), 2)]
-            for i in range(0, len(polygon_points)-1, 2):
-                cv2.rectangle(image, polygon_points[i], polygon_points[i+1], (0, 255, 0), 2)
-            cv2.putText(image, str(class_id), polygon_points[0], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+            
+            # Drawing the polygon
+            cv2.polylines(image, [np.array(polygon_points)], isClosed=True, color=(255, 255, 255), thickness=2)
+            cv2.putText(image, str(class_id), polygon_points[0], cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
 
     # Save the image to the destination
-    destination_path = "/mnt/ssd-cluster/martin/cv4e/conversion_test"
+    destination_path = "/Users/mmarzi/Desktop/conversion_test"
     filename_on_desktop = os.path.join(destination_path, image_name + "_annotated.jpg")
 
     # Check if the directory exists
@@ -41,7 +42,9 @@ def display_yolo_segmentations(image_dir, yolo_annotations_dir, image_name):
         print(f"Failed to save the image at: {filename_on_desktop}")
 
 # Example usage
-image_directory = "/mnt/ssd-cluster/martin/datasets/mask/images/test"
-yolo_annotation_directory = "/mnt/ssd-cluster/martin/datasets/mask/labels/test"
-sample_image_name = "Pn_calliantha_20230523_0DB2_1"
+import numpy as np  # Ensure you import numpy
+
+image_directory = "/Users/mmarzi/MLprojects/cv4e/datasets/data_mask/processed/images/test"
+yolo_annotation_directory = "/Users/mmarzi/MLprojects/cv4e/datasets/data_mask/processed/labels/test"
+sample_image_name = "Pn_delicatissima_20230601_0035_35"
 display_yolo_segmentations(image_directory, yolo_annotation_directory, sample_image_name)
