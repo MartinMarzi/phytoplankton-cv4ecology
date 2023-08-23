c04e6295e65d74758ef39d245bde43d75b37561a
martin <marzidovsek@gmail.com>
2023-08-23 22:06:49 +0000

    trainings

--- pp_object_detection/train.py
+++ pp_object_detection/train.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')
 
 # Train the model
-results = model.train(data="pp.yaml", epochs=80, imgsz=640)
+results = model.train(data="pp.yaml", epochs=10, cfg="default.yaml", imgsz=640)
--- pp_segmentation/predict.py
+++ pp_segmentation/predict.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
 # Load model
-model = YOLO("/home/martin/cv4e/pp_segmentation/runs/segment/train7/weights/best.pt")
+model = YOLO("/home/martin/cv4e/pp_segmentation/runs/segment/train16/weights/best.pt")
 
 # Define path to the image file
-source = "/mnt/ssd-cluster/martin/datasets/mask/images/test/Pn_calliantha_20230601_0DB2_35.jpg"
+source = "/mnt/ssd-cluster/martin/datasets/mask/images/test/Pn_calliantha_20230523_0DB2_1.jpg"
 
 # Predict with the model
 results = model(source,  save=True)
