a0e244f49ee1d0151d93818526100f82e09615ea
martin <marzidovsek@gmail.com>
2023-08-23 22:06:31 +0000

    updated configs, trainings

--- pp_segmentation/train.py
+++ pp_segmentation/train.py
@@ -1,8 +1,15 @@
+import comet_ml 
+import os
 from ultralytics import YOLO
 
+COMET_API_KEY = "oL5iq9NEaBxQ2y6s9LSKz8NuV"
+os.environ["COMET_API_KEY"]= COMET_API_KEY
+
+# initialize experiment in comet
+comet_ml.init("pp_seg")
+
 # Load a model
-model = YOLO("yolov8n-seg.pt")
+model = YOLO("yolov8s-seg.pt")
 
 # Train the model
-results = model.train(data="pp.yaml", epochs=30, imgsz=640)
-
+results = m/home/martin/cv4e/pp_segmentation/wandbodel.train(data="pp.yaml", epochs=1, imgsz=640, cfg="config.yaml")
