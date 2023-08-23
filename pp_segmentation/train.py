3350336754b4f5a0b714cef4d9bdc1687f46a6a2
martin <marzidovsek@gmail.com>
2023-08-23 00:25:47 +0000

    added validation

--- pp_segmentation/train.py
+++ pp_segmentation/train.py
@@ -1,7 +1,8 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8n-seg.pt")
+model = YOLO("yolov8s-seg.pt")
 
 # Train the model
-results = model.train(data="pp.yaml", epochs=100, imgsz=640)
+results = model.train(data="pp.yaml", epochs=100, imgsz=640, plot=True)
+
