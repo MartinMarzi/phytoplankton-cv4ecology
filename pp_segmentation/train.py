5573ac7be9e505c00983083edbf0281d44af365c
martin <marzidovsek@gmail.com>
2023-08-23 00:53:57 +0000

    updated

--- pp_segmentation/train.py
+++ pp_segmentation/train.py
@@ -1,8 +1,8 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8s-seg.pt")
+model = YOLO("yolov8n-seg.pt")
 
 # Train the model
-results = model.train(data="pp.yaml", epochs=100, imgsz=640, plot=True)
+results = model.train(data="pp.yaml", epochs=30, imgsz=640)
 
