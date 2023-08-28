b64c3445c6cf47fca612a69b5bec9b3ebd737244
martin <marzidovsek@gmail.com>
2023-08-28 21:18:41 +0000

    validation, removed old runs

--- pp_segmentation/val.py
+++ pp_segmentation/val.py
@@ -1,11 +1,18 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("/home/martin/cv4e/pp_segmentation/pp_segment/train/weights/best.pt")
+model = YOLO("/home/martin/cv4e/pp_segmentation/pp_segment/train3/weights/best.pt")
 
 # Validate the model
-metrics = model.val(save_json=True, cfg="config.yaml")
-metrics.box.map
+metrics = model.val(
+    save_json=True,  
+    cfg="config.yaml")
+
+metrics.box.map    # map50-95(B)
+metrics.box.map50  # map50(B)
+metrics.box.map75  # map75(B)
 metrics.box.maps   # a list contains map50-95(B) of each category
-metrics.seg.map 
-metrics.seg.maps 
+metrics.seg.map    # map50-95(M)
+metrics.seg.map50  # map50(M)
+metrics.seg.map75  # map75(M)
+metrics.seg.maps   # a list contains map50-95(M) of each category
