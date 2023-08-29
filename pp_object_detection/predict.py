e7542480372755266e30450f8d1492bc1d589367
martin <marzidovsek@gmail.com>
2023-08-29 00:55:17 +0000

    updates

--- pp_object_detection/predict.py
+++ pp_object_detection/predict.py
@@ -1,10 +1,10 @@
 from ultralytics import YOLO
 
 # Load  a model
-model = YOLO("/home/martin/cv4e/pp_object_detection/runs/detect/train8/weights/best.pt")
+model = YOLO("/home/martin/cv4e/pp_object_detection/pp_detect/train2/weights/best.pt")
 
 # Define path to the image file
 source = "/mnt/ssd-cluster/martin/datasets/bbox/images/test/Pn_delicatissima_20230601_0DB2_6.jpg"
 
 # Predict with the model
-results = model(source, save=True) 
+results = model(source, save=True, save_txt=True) 
