d3ae263470d39ce37e1d653c66d383b843c59525
martin <marzidovsek@gmail.com>
2023-08-31 19:45:11 +0000

    udpte predict function with mask visualisaiton

--- pp_segmentation/predict.py
+++ pp_segmentation/predict.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 import ipdb
+import numpy as np
 
 # Load model
 model = YOLO(
@@ -7,14 +8,15 @@ model = YOLO(
     )
 
 # Define path to the image file
-source = "/mnt/ssd-cluster/martin/datasets/mask/images/test/Pn_calliantha_20230523_0DB2_1.jpg"
+# source = "/mnt/ssd-cluster/martin/datasets/mask/images/test/Pn_calliantha_20230523_0DB2_1.jpg"
+source = "/mnt/ssd-cluster/martin/datasets/mask/images/holdout/Pn_calliantha_20230601_0DB2_11.jpg"
 
 # Predict with the model
 results = model(
     source, 
     save=True, 
     save_txt = True,
-    save_conf=False,
+    save_conf=True,
     retina_masks=True, 
     imgsz=1920,
     iou = 0.2,
@@ -22,15 +24,30 @@ results = model(
     cfg="config.yaml"
     ) # show=True, 
 
+print (type(results))
 # View results
-for result in results:
-    masks = result.masks # Masks object for segmentation masks outputs
-    for mask in masks.xyn:
-        print(mask)
-    # print(f"mask {count}\n{masks}")  # print the Masks object containing the detected instance masks
-    # print(f"mask nr. pixels: {len(masks)}")  # print the Masks object containing the detected instance masks
-    # print(f"box: {result.boxes.xyxy}")  # print the Boxes object containing the detection bounding boxes
-    
-    # ipdb.set_trace()
+result =results[0]
+
+shit = result.masks.cpu() # Masks object for segmentation masks outputs
+masks = shit.data.numpy()
+print (f"type is {type(masks)} and shape is {masks.shape}")
+num_masks = masks.shape[0]
+print (f"Number of masks detected is {num_masks}")
+for idx in range(0,num_masks):
+    print (f"Mask number: {idx+1}")
+    individual_mask = masks[idx]
+    plt.imshow(individual_mask)
+    x_ones,y_ones = np.where(individual_mask == 1)
+    number_of_ones = x_ones.shape[0]
+    print (f"Number of pixels is {number_of_ones}")
+    break
+
+#for mask in masks.xyn:
+#    print(mask)
+# print(f"mask {count}\n{masks}")  # print the Masks object containing the detected instance masks
+# print(f"mask nr. pixels: {len(masks)}")  # print the Masks object containing the detected instance masks
+# print(f"box: {result.boxes.xyxy}")  # print the Boxes object containing the detection bounding boxes
+
+# ipdb.set_trace()
 
 
